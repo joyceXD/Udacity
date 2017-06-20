80d7a6e39e66fd4312a187fa9e670029b98d8c47
XINPANG\X.Pang <joyce.xin.pang@gmail.com>
2017-06-20 23:58:28 +0200

    Small updates on Project 5 and documentation.

--- 5_IdentifyFraudFromEnronEmail/final_project/poi_id.py
+++ 5_IdentifyFraudFromEnronEmail/final_project/poi_id.py
@@ -20,32 +20,32 @@ features_list = ['poi', 'salary', 'to_messages', 'deferral_payments', 'total_pay
                  'from_messages', 'other', 'from_this_person_to_poi', 'director_fees', 'deferred_income',
                  'long_term_incentive', 'from_poi_to_this_person']  # 'email_address' is excluded
 
-
 # Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-# Task 2: Remove outliers
-# Task 3: Create new feature(s)
 # Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 # Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, features_list, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
+# Convert to pandas dataframe
 features = pd.DataFrame(features)
 
-# print features.describe()
+# Explore and visualize statistics of each column to find outliers
+print features.describe()
 
 for i in range(1, len(features.columns)):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.boxplot(features.iloc[:, i-1])
+    ax.boxplot(features.iloc[:, i - 1])
     ax.set_xlabel(features_list[i])
     plt.savefig('./plot/{}.png'.format(features_list[i]))
 
-
+# Task 2: Remove outliers
+# Task 3: Create new feature(s)
 
 # Task 4: Try a variaty of classifiers
 # Please name your classifier clf for easy export below.
@@ -55,6 +55,7 @@ for i in range(1, len(features.columns)):
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
+
 clf = GaussianNB()
 
 # Task 5: Tune your classifier to achieve better than .3 precision and recall
@@ -66,6 +67,7 @@ clf = GaussianNB()
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
+
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
