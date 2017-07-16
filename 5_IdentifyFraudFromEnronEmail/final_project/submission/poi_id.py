6259a40a5a11eb6b17f87afc0adfc487ab142f76
XINPANG\X.Pang <joyce.xin.pang@gmail.com>
2017-07-17 00:23:59 +0200

    Project 5 version 2 after review.

--- 5_IdentifyFraudFromEnronEmail/final_project/submission/poi_id.py
+++ 5_IdentifyFraudFromEnronEmail/final_project/submission/poi_id.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python
 
 
-# Functions to be used in this project
 # We use SVM classifier and tune the parameters
-def build_model_svm(features_std, param_grid):
+def build_model_svm(features_std):
     # the input features should be standardized already
     x_train, x_test, y_train, y_test = train_test_split(features_std, labels, test_size=0.2, random_state=0)
+
     cv_svm = StratifiedShuffleSplit(n_splits=5, test_size=0.3, random_state=99)
-    grid = GridSearchCV(svm.SVC(), cv=cv_svm, n_jobs=1, param_grid=param_grid, scoring='precision')
+
+    param_grid = [
+        {'C': [100],
+         'gamma': [0.01, 0.001, 0.0001],
+         'kernel': ['rbf'],
+         'class_weight': [{0: 0.1}]
+        },
+    ]
+    grid = GridSearchCV(svm.SVC(), cv=cv_svm, n_jobs=1, param_grid=param_grid, scoring='recall')
     grid.fit(x_train, y_train)
     return grid
-
-
-def calc_ratio(value, total):
-    if total == 0:
-        return 0
-    else:
-        return float(float(value) / float(total))
-
-
-def visualize_features(features):
-    for i in range(1, len(features.columns)):
-        plt.hist(features.iloc[:, i - 1], 100, facecolor='green', alpha=0.75)
-        plt.xlabel(features_list[i])
-        plt.savefig('./plot/hist_{}.png'.format(features_list[i]))
-
-
-def visualize_mutual_info(kscores):
-    ind = np.arange(len(kscores['score']))
-    width = 0.35  # define width of the bars
-    plt.bar(ind, kscores['score'], width, color='r')
-    plt.xticks(ind + width / 2, list(kscores.index), rotation="vertical")
-    plt.title('Mutual information score for each feature')
-    plt.tight_layout()
-    plt.show()
-
-
-def visualize_outliers(df_plot):
-    c_map = {1: 'r', 0: 'b'}
-    plt.scatter(df_plot['total_stock_value'], df_plot['total_payments'],
-                c=[c_map.get(c, 'yellow') for c in df_plot.POI], alpha=0.5)
-    plt.title('Scatter plot of total stock value and total payments')
-    plt.xlabel('Total stock value')
-    plt.ylabel('Total payments')
-    plt.show()
-
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -62,6 +35,15 @@ if __name__ == '__main__':
 
     from feature_format import featureFormat, targetFeatureSplit
     from tester import dump_classifier_and_data, test_classifier
+
+
+    # Functions to be used in this project
+    def calc_ratio(value, total):
+        if total == 0:
+            return 0
+        else:
+            return float(float(value) / float(total))
+
 
     # Task 1: Select what features you'll use.
     # features_list is a list of strings, each of which is a feature name.
@@ -91,19 +73,17 @@ if __name__ == '__main__':
 
     # Convert to pandas data frame for easier pre-processing and visualization
     features = pd.DataFrame(features)
-    features.columns = features_list[1:]
+
+    # for i in range(1, len(features.columns)):
+    #     # fig = plt.figure()
+    #     # ax = fig.add_subplot(111)
+    #     # ax.boxplot(features.iloc[:, i - 1])
+    #     plt.hist(features.iloc[:, i - 1], 100, facecolor='green', alpha=0.75)
+    #     plt.xlabel(features_list[i])
+    #     plt.savefig('./plot/hist_{}.png'.format(features_list[i]))
 
     # Task 2: Remove outliers
-    # plot total payments vs. total stock value
-    df_plot = features
-    df_plot['POI'] = labels
-    # visualize_outliers(df_plot)
-
-    # outlier removal
-    df_plot = df_plot[df_plot.total_stock_value <= 100000000]
-    df_plot = df_plot[df_plot.total_payments <= 100000000]
-    labels = list(df_plot['POI'])
-    features = df_plot.drop(['POI'], axis=1)
+    features.columns = features_list[1:]
 
     # Task 3: Create new feature(s)
     features['from_this_person_to_poi_fraction'] = features.apply(lambda row: calc_ratio(row['from_this_person_to_poi'],
@@ -139,12 +119,22 @@ if __name__ == '__main__':
     kscores = kscores.sort_values('score', ascending=0)
     # print "Mutual information for each feature: ", kscores
 
+    """ Visualize mutual information for each feature """
+    # ind = np.arange(len(fs_kbest.scores_))
+    # width = 0.35       # the width of the bars
+    # fig = plt.figure()
+    # plt.bar(ind, kscores['score'], width, color='r')
+    # plt.xticks(ind + width / 2, list(kscores.index), rotation="vertical")
+    # plt.title('Mutual information score for each feature')
+    # plt.tight_layout()
+    # plt.show()
+
     # Define list of selected features
-    selected_feature_names = ['bonus', 'deferred_income', 'exercised_stock_ratio',
-                              'expenses', 'from_this_person_to_poi',
-                              'from_this_person_to_poi_fraction', 'from_poi_to_this_person_fraction',
-                              'other', 'restricted_stock', 'restricted_stock_deferred',
-                              'salary', 'shared_receipt_with_poi', 'total_stock_value']
+    selected_feature_names = ['salary', 'total_payments', 'from_this_person_to_poi_fraction',
+                              'from_poi_to_this_person_fraction', 'bonus', 'restricted_stock',
+                              'shared_receipt_with_poi',
+                              'exercised_stock_ratio', 'restricted_stock_deferred', 'total_stock_value', 'expenses',
+                              'other', 'from_this_person_to_poi', 'deferred_income', 'from_poi_to_this_person']
     selected_features = features[selected_feature_names]
     # print 'Pearson correlation between feature pairs: ', selected_features.corr(method='pearson')
 
@@ -155,37 +145,21 @@ if __name__ == '__main__':
     features_std_svm = preprocessing.scale(selected_features)
     clf_svm = svm.SVC(class_weight={0: 0.124, 1: 0.876})
     cv_scores = cross_val_score(clf_svm, features_std_svm, labels, cv=cv, scoring='f1')
-    # print "Support Vector Machine: F1 scores after cross validation are: ", cv_scores
+    print "Support Vector Machine: F1 scores after cross validation are: ", cv_scores
 
     # Naive Bayes classifier
-    selected_feature_names_nb = ['exercised_stock_ratio', 'from_this_person_to_poi',
-                                 'from_this_person_to_poi_fraction',
+    selected_feature_names_nb = ['total_payments', 'from_this_person_to_poi_fraction',
                                  'from_poi_to_this_person_fraction',
-                                 'restricted_stock_deferred', 'shared_receipt_with_poi']
+                                 'shared_receipt_with_poi', 'exercised_stock_ratio', 'restricted_stock_deferred',
+                                 'from_this_person_to_poi', 'from_poi_to_this_person']
     features_nb = selected_features[selected_feature_names_nb]
     features_std_nb = preprocessing.scale(features_nb)
     clf_nb = naive_bayes.GaussianNB()
     cv_scores = cross_val_score(clf_nb, features_std_nb, labels, cv=cv, scoring='f1')
-    # print "Naive Bayes: F1 scores after cross validation are: ", cv_scores
+    print "Naive Bayes: F1 scores after cross validation are: ", cv_scores
 
-    param_grid_1 = [
-        {'C': [1, 100],
-         'gamma': [0.001, 0.0001],
-         'kernel': ['rbf'],
-         'class_weight': [{0: 0.1}, {0: 0.2}, {0: 0.5}, {0: 0.8}]
-         }
-    ]
-    param_grid_2 = [
-        {'C': [100],
-         'gamma': [0.1, 0.01, 0.001, 0.0001],
-         'kernel': ['rbf'],
-         'class_weight': [{0: 0.2}]
-        }
-    ]
-
-    grid_results_1 = build_model_svm(features_std_svm, param_grid_1)
-    grid_results_2 = build_model_svm(features_std_svm, param_grid_2)
-    # print grid_results_2.best_params_
+    grid_results = build_model_svm(features_std_svm)
+    print grid_results.best_params_
 
     # Task 5: Tune your classifier to achieve better than .3 precision and recall
     # using our testing script. Check the tester.py script in the final project
@@ -193,7 +167,7 @@ if __name__ == '__main__':
     # function. Because of the small size of the dataset, the script uses
     # stratified shuffle split cross validation. For more info:
     # http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-    clf = svm.SVC(kernel='rbf', C=100, gamma=0.01, class_weight={0: 0.2, 1: 0.8})
+    clf = svm.SVC(kernel='rbf', C=100, gamma=0.01, class_weight={0:0.1, 1:0.9})
 
     features_list = ['poi'] + selected_feature_names
     my_dataset = {}
@@ -206,3 +180,4 @@ if __name__ == '__main__':
     # # that the version of poi_id.py that you submit can be run on its own and
     # # generates the necessary .pkl files for validating your results.
     dump_classifier_and_data(clf, my_dataset, features_list)
+    # test_classifier(clf, my_dataset, features_list, folds=1000)
